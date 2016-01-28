a2b08e0d165966f946544e1add85aa48ef0a3b44
Giulio <giulio.pietroiusti@gmail.com>
2016-01-28 23:54:15 +0100

    no colors...

--- public/display.php
+++ public/display.php
@@ -6,7 +6,13 @@
     //store tricks rows of the spot in $tricks 
     $tricks = query("SELECT skater, trick, source, link FROM tricks WHERE id = ?", $_GET["id"]);
     $counter = 0;
-    
+   
+    //sort tricks by skater
+       
+ 
+    //set default tricks to be sorted by skater
+  //  $tricks = $by_skater;
+ 
     //display tricks
     render2("../templates/display_template.php", ["tricks" => $tricks, "counter" => $counter, "title" => "Tricks history"]);
 
--- templates/display_template.php
+++ templates/display_template.php
@@ -1,9 +1,9 @@
 <div id="sortingBar">
 	<form>
 		Sorty by: 
-		<input type="radio" name="index" value="votes" checked="checked">votes</input>
-		<input type="radio" name="index" value="skateboarder">skateboarder</input>
-		<input type="radio" name="index" value="year">year</input> 
+		<input type="radio" name="votes" value="votes" checked="checked">votes</input>
+		<input type="radio" name="skateboarder" value="skateboarder">skateboarder</input>
+		<input type="radio" name="year" value="year">year</input> 
 	</form>
 </div>
 <div>
@@ -17,46 +17,14 @@
             </tr>
         </thead>
         <tbody style="text-align: left;">
-            <?php 
-                foreach ($tricks as $trick) {
-                    $skater = $trick["skater"];
-                    $tricko = $trick["trick"];
-                    $source = $trick["source"];
-                    $link = $trick["link"];    
-
-                    $target = '"_blank"';
-                    
-                    $info = '"info"';
-                    $success = '"success"';
-                    $danger = '"danger"';
-                    $warning = '"warning"';
-                    $active = '"active"';
-
-
-                    if ( $counter == 0 || $counter == 1)
-            		     echo "<tr>";
-                    else if ($counter == 2)
-                         echo "<tr class=$info>";
-                    else if ($counter == 3)
-                         echo "<tr class=$success>";
-                    else if ($counter == 4)
-                         echo "<tr class=$danger>";
-                    else if ($counter == 5)
-                         echo "<tr class=$warning>";
-                    else if ($counter == 6)
-                         echo "<tr class=$active>";
-                    echo "<td>$skater</td>";
-                    echo "<td>$tricko</td>";
-                    echo "<td>$source</td>";
-                    echo "<td><a href=$link target=$target>&#x21E8</a></td>";
-                	echo "</tr>";
-
-                    $counter = $counter + 1; 
-                    if ($counter == 6) {
-                        $counter = 0; 
-                    }
-                }     
-            ?>
+            <?php foreach ($tricks as $trick): ?>
+                <tr>
+                    <td><?= $trick['skater'] ?></td>
+                    <td><?= $trick["trick"] ?></td>
+                    <td><?= $trick["source"] ?></td>
+                    <td><a href=<?= $trick["link"] ?> target="_blank">&#x21E8</a></td>
+                </tr> 
+            <?php endforeach ?>
         </tbody>
     </table>
 
