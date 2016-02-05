d8344d982e8cebb81311b6e39918943f793d2f35
Giulio <giulio.pietroiusti@gmail.com>
2016-02-05 19:31:56 +0100

    add css rules

--- templates/about_template.php
+++ templates/about_template.php
@@ -1,15 +1,15 @@
 
-
+<div id="column1">
 <h3>A free tricks database</h3>
 <p class="text-info">In Sk8 archives we collect facts of skateboarding history, we're building a collection of the best tricks landed at iconic skate spots. We think that it is easier to collect this data if we do it all togheter. You are welcome to join us and insert tricks that are not present yet.</p>
+</div>
 
-
-
+<div id="column2">
 <h3>Collaborate</h3>
 <p class="text-info">Collaboration is the key factor in order to collect the maximum amount of data. If everyone shares his knowledge we will built the best possible database of tricks. So we ask you to insert tricks if you notice any lack. You are also welcome to write us for any comment or suggestion.</p>
+</div>
 
-
-
+<div id="column3">
 <h3>Rules</h3>
 <p class="text-info">If you want to insert a trick then you need to provide us with, besides the name of the trick, the name of the skater and the source. The source may be a photo magazine as well as a video, but we can accept something else too (for example a verbal testimonial in a interview).</p>
-
+</div>
